36a2ebb49d63076a1fa6f85542922bf704797f4f
Apple <Vivek.Pawar@humanebits.com>
2024-08-26 13:24:52 +0530

    First commit

---
+++ MauiDrawing/testview.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiDrawing;
+
+public partial class testview : ContentPage
+{
+	public testview()
+	{
+		InitializeComponent();
+	}
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+         Shell.Current.GoToAsync($"//{nameof(MainPage)}");
+    }
+}
